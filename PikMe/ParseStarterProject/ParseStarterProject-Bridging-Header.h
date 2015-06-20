37c4094f6255ee46b23aa4a5538c90746777feb4
FauziCode <fauzi.qasem@gmail.com>
2015-06-20 11:51:49 +0200

    Rename Project

--- PikMe/ParseStarterProject/ParseStarterProject-Bridging-Header.h
+++ PikMe/ParseStarterProject/ParseStarterProject-Bridging-Header.h
@@ -4,8 +4,8 @@
 //  Copyright 2011-present Parse Inc. All rights reserved.
 //
 
-#ifndef ParseStarterProject_Bridging_Header_h
-#define ParseStarterProject_Bridging_Header_h
+#ifndef PikMe_Bridging_Header_h
+#define PikMe_Bridging_Header_h
 
 // If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
 //#import <ParseFacebookUtils/PFFacebookUtils.h>
