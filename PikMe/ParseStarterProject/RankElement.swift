7f1504e32cdee711bb8c4a6c72125c8af271d2ec
Giacomo Santarnecchi <giacomosantarnecchi@hotmail.com>
2015-09-14 20:53:49 +0200

    Bozza di classifica

---
+++ PikMe/ParseStarterProject/RankElement.swift
@@ -0,0 +1,16 @@
+//
+//  RankElement.swift
+//  PikMe
+//
+//  Created by Giacomo on 14/09/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class RankElement: NSObject {
+    
+    var username : String = ""
+    var totLikeN : Int = 0
+    var position : Int = 0
+}
